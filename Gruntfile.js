5ed1ab230faf65df51d4b6458160561b0c2e1231
StianBPedersen <stianb@kompis.com>
2014-03-11 15:28:30 +0100

    add uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,32 @@ module.exports = function(grunt) {
         },
       }
 		},
+		uglify: {
+			compress: {
+				options: {
+					mangle: true,
+					preserveComments: false,
+					drop_console: true
+				},
+				files: {
+					'assets/js/script.min.js': ['assets/js/script.js']
+				}
+			}
+		},
 		clean: {
 			files: ['assets/css/style.css']
 		},
 		watch: {
 			css: {
 				files: ['assets/scss/*.scss', '**/*.hbs'],
-				tasks: ['clean', 'sass:compile'],
+				tasks: ['clean', 'sass:compile',],
+				options: {
+					livereload: true
+				}
+			},
+			js: {
+				files: ['assets/js/script.js'],
+				tasks: ['uglify'],
 				options: {
 					livereload: true
 				}
@@ -29,6 +48,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.registerTask('default', ['clean', 'sass']);
 	grunt.registerTask('w', ['watch']);
